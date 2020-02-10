d83314868b74a6469169a6a954f6ddd5f68c4d69
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2020-02-10 13:52:53 +0100

    till the first factory steps: 2 controller 1 service 2 view

---
+++ app/controllers/ordersController.js
@@ -0,0 +1,18 @@
+(function() {
+
+    function OrdersController($scope, $routeParams, customersFactory) {
+
+        var customerId = $routeParams.customerId;
+        $scope.customer = null;
+
+        function init() {
+            $scope.customer = customersFactory.getCustomer(customerId)
+        }
+        init()
+    }
+
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersFactory']
+
+    angular.module('customersApp').controller('OrdersController', OrdersController);
+
+}());
